5fe66946ce74d2b999d09adeb5400cd96d0ce291
n4zmxxn <you@example.com>
2023-08-01 08:53:35 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -198,8 +198,9 @@ resource "aws_key_pair" "hashicat" {
   public_key = tls_private_key.hashicat.public_key_openssh
 }
 
-module "terraform-aws-s3-bucket"{
+module "s3-bucket" {
   source              = "app.terraform.io/AfternoonWorkspace-14daytrial/s3-bucket/aws"
   version             = "3.1.0"
   s3_object_ownership = "BucketOwnerEnforced"
 }
+
